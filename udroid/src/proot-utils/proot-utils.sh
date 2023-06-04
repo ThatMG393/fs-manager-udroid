33c9e8f03c12d919cc2fefdb0b444235906fa6ef
ThatMG393 <thatmg393@gmail.com>
2023-06-04 10:53:26 +0800

    improve code quality

--- udroid/src/proot-utils/proot-utils.sh
+++ udroid/src/proot-utils/proot-utils.sh
@@ -127,8 +127,8 @@ if [ -n "$RUN_STANDALONE" ]; then
     while [ $# -gt 0 ]; do
         case $1 in
             --install|-i)   ;;
-            --login|-l) shift 1; login $*; break    ;;
-            --extract)  shift 1; extract $*; break  ;;
+            --login|-l) shift 1; login "$@"; break    ;;
+            --extract)  shift 1; extract "$@"; break  ;;
             --uninstall|-u) ;;
         esac
     done
